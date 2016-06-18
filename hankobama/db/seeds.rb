b0476ece5b14b1c079cd0231ae4dc7b303d0a411
Hank860502 <hank860502@gmail.com>
2016-06-18 15:25:57 -0700

    finally works, seed obamafaker

--- hankobama/db/seeds.rb
+++ hankobama/db/seeds.rb
@@ -2,3 +2,13 @@
 	Article.create(sentence: Faker::Hacker.say_something_smart, image_url: Faker::Avatar.image)
 		)
 end
+
+10.times do(
+	Article.create(sentence: Obama::Saying.sentence, image_url: Obama::Image.serious)
+		)
+end
+
+10.times do(
+	Article.create(sentence: Obama::Saying.sentence, image_url: Obama::Image.funny)
+		)
+end
